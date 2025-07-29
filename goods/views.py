dfd2a7a96d8d699d0184eef6c55c63aefb0786d0
Никита <nekitos1011@gmail.com>
2025-07-30 02:18:38 +0300

    Add product filters in catalog

--- goods/views.py
+++ goods/views.py
@@ -1,21 +1,30 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from goods.models import Categories, Products
-# Create your views here.
 
 
 def catalog(request, category_slug):
+
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
     if category_slug == "all":
         goods = Products.objects.all()
     else:
-        goods = get_list_or_404(Products, category__slug=category_slug)
+        goods = Products.objects.filter(category__slug=category_slug)
 
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
     paginator = Paginator(goods, 3)
     page = request.GET.get("page", 1)
     current_page = paginator.get_page(int(page))
+
     context = {
         "title": "ModaHouse - Каталог",
         "goods": current_page,
+        "slug_url": category_slug
     }
 
     return render(request, "goods/catalog.html", context=context)
