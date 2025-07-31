f30d7daf1de3ac6e93d7c181f2b453a701a1af17
Никита <nekitos1011@gmail.com>
2025-07-31 04:39:12 +0300

    Add user login functionality

---
+++ users/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class UserLoginForm(AuthenticationForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "password"]
