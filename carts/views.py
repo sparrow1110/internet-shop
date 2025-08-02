ec973bf28df05290dca1c2318f993cefb3429148
Никита <nekitos1011@gmail.com>
2025-08-02 03:53:39 +0300

    Implement AJAX add to cart without page reload

--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from carts.models import Cart
+from carts.utils import get_user_carts
 from goods.models import Products
 
 
-def cart_add(request, product_slug):
-    product = Products.objects.get(slug=product_slug)
+def cart_add(request):
+    product_id = request.POST.get("product_id")
+    product = Products.objects.get(pk=product_id)
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
         if carts.exists():
@@ -16,14 +20,22 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string("carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Товар добавлен в корзину!",
+        "cart_items_html": cart_items_html
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
     pass
 
 
-def cart_remove(request, cart_id):
+def cart_remove(request):
     cart = Cart.objects.get(id=cart_id)
     cart.delete()
     return redirect(request.META['HTTP_REFERER'])
